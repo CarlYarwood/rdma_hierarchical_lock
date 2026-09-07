3d87618b416c4cd1ad4d83c751884bd93c4f4922
CarlYarwood <cdy8858@truman.edu>
2026-09-07 16:50:02 +0100

    sf

--- src/mcs_client.c
+++ src/mcs_client.c
@@ -554,7 +554,7 @@ void* mcs_client(void *in) {
 		}
 	}
 
-    printf("node id %lu after wait", *node_id);
+    printf("node id %lu after wait\n", *node_id);
 	for (int i = (*node_id) - 1; i>=0 ; i--) {
         disconnect_client(cm_event_channel, id_arr[i]);
 		id_arr[i] = NULL;
