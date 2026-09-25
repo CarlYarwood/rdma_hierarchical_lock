2364428d1c0c6f65e4cfcfb099aba7a641071004
CarlYarwood <cdy8858@truman.edu>
2026-09-25 14:55:07 -0500

    sf

--- src/rdma_node.h
+++ src/rdma_node.h
@@ -16,8 +16,8 @@ typedef struct {
     char * parent_types;
     char ** parent_addresses;
     long * parent_ports;
-    char *** peer_addresses;
-    long ** peer_ports;
+    char ** peer_addresses[31];
+    long * peer_ports[31];
     int * num_peers;
 } client_config;
 
